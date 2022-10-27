5eebdef46bdcdac0e451ce9f6f221f5be460e1ef
milenarismo <milenasimoeslabenu@gmail.com>
2022-10-27 12:35:26 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -2,78 +2,161 @@
 
 // Primeira:
 
-const nomeSerie1 = "Game of Thrones"
-const genero1 = "Ficção Científica"
-const duracaoTemp1 = Number(8)
-const anoLancamento1 = Number(2011)
-const remake1 = false
-const estiloSerie1 = ["Disputa por poder", "guerras épicas", "dragões"]
-console.log(`Nome da Série: ${nomeSerie1.toUpperCase()}
-Gênero: ${genero1}
-Quantidade de Temporadas: ${duracaoTemp1}
-Ano de Lançamento: ${anoLancamento1}
-É remake de alguma série? ${remake1}
-Essa série gira em torno de: ${estiloSerie1}`)
+const serie1 = {
+Nome: "Game of Thrones",
+Gênero: "Ficção Científica",
+Temporadas: Number(8),
+Lançamento: Number(2011),
+Remake: true,
+Estilo: "Disputa por poder,guerras épicas,dragões"}
+
+// console.log("Nome da Série:", serie1.nomeSerie.toUpperCase(),
+// "Gênero:", serie1.genero,
+// "Quantidade de Temporadas:", serie1.duracaoTemp,
+// "Ano de Lançamento:", serie1.anoLancamento,
+// "É remake de alguma série?", serie1.remake,
+// "Essa série gira em torno de:", serie1.estiloSerie)
+
+for(let i in serie1){
+  console.log(i,serie1[i])}
+
+const funcao = (objeto) => {
+    const string = []
+  string.push(objeto.Estilo)
+ return string
+ }
+console.log(funcao(serie1))
+
+
 
 // Segunda:
 
-const nomeSerie2 = "Breaking Bad"
-const genero2 = "Ação"
-const duracaoTemp2 = Number(5)
-const anoLancamento2 = Number(2008)
-const remake2 = false
-const estiloSerie2 = ["Tráfico", "violência", "drama"]
-console.log(`Nome da Série: ${nomeSerie2.toUpperCase()}
-Gênero: ${genero2}
-Quantidade de Temporadas: ${duracaoTemp2}
-Ano de Lançamento: ${anoLancamento2}
-É remake de alguma série? ${remake2}
-Essa série gira em torno de: ${estiloSerie2}`)
+const serie2 = {
+Nome: "Breaking Bad",
+Gênero: "Ação",
+Temporadas: Number(5),
+Lançamento: Number(2008),
+Remake: true,
+Estilo: "Tráfico, violência,drama"}
+
+// console.log("Nome da Série:", serie2.nomeSerie.toUpperCase(),
+// "Gênero:", serie2.genero,
+// "Quantidade de Temporadas:", serie2.duracaoTemp,
+// "Ano de Lançamento:", serie2.anoLancamento,
+// "É remake de alguma série?", serie2.remake,
+// "Essa série gira em torno de:", serie2.estiloSerie)
+
+for(let i in serie2){
+  console.log(i,serie2[i])}
+
+  const funcao2 = (objeto) => {
+    const string = []
+    string.push(objeto.Estilo)
+    return string
+  }
+  
+  console.log(funcao(serie2))
+
+
 
 // Terceira:
 
-const nomeSerie3 = "Prinson Break"
-const genero3 = "Suspense"
-const duracaoTemp3 = Number(4)
-const anoLancamento3 = Number(2005)
-const remake3 = false
-const estiloSerie3 = ["Crimes", "injustiça", "sistema carcerário"]
-console.log(`Nome da Série: ${nomeSerie3.toUpperCase()}
-Gênero: ${genero3}
-Quantidade de Temporadas: ${duracaoTemp3}
-Ano de Lançamento: ${anoLancamento3}
-É remake de alguma série? ${remake3}
-Essa série gira em torno de: ${estiloSerie3}`)
+const serie3 = {
+Nome: "Prinson Break",
+Gênero: "Suspense",
+Temporadas: Number(4),
+Lançamento: Number(2005),
+Remake: true,
+Estilo: "Crimes,injustiça, sistema carcerário"}
+
+// console.log("Nome da Série:", serie3.nomeSerie.toUpperCase(),
+// "Gênero:", serie3.genero,
+// "Quantidade de Temporadas:", serie3.duracaoTemp,
+// "Ano de Lançamento:", serie3.anoLancamento,
+// "É remake de alguma série?", serie3.remake,
+// "Essa série gira em torno de:", serie3.estiloSerie)
+
+for(let i in serie3){
+  console.log(i,serie3[i])}
+
+
+  const funcao3 = (objeto) => {
+    const string = []
+    string.push(objeto.Estilo)
+    return string
+  }
+  
+  console.log(funcao(serie3))
+
+
+
+
 
 //Quarta:
 
-const nomeSerie4 = "Stranger Things"
-const genero4 = "Aventura"
-const duracaoTemp4 = Number(4)
-const anoLancamento4 = Number(2015)
-const remake4 = false
-const estiloSerie4 = ["Monstros", "mundo paralelo", "super poderes"]
-console.log(`Nome da Série: ${nomeSerie4.toUpperCase()}
-Gênero: ${genero4}
-Quantidade de Temporadas: ${duracaoTemp4}
-Ano de Lançamento: ${anoLancamento4}
-É remake de alguma série? ${remake4}
-Essa série gira em torno de: ${estiloSerie4}`)
+const serie4 = {
+Nome: "Stranger Things",
+Gênero: "Aventura",
+Temporadas: Number(4),
+Lançamento: Number(2015),
+Remake: true,
+Estilo: "Monstros, mundo paralelo, super poderes"}
+
+// console.log("Nome da Série:", serie4.nomeSerie.toUpperCase(),
+// "Gênero:", serie4.genero,
+// "Quantidade de Temporadas:", serie4.duracaoTemp,
+// "Ano de Lançamento:", serie4.anoLancamento,
+// "É remake de alguma série?", serie4.remake,
+// "Essa série gira em torno de:", serie4.estiloSerie)
+
+for(let i in serie4){
+  console.log(i,serie4[i])}
+
+
+  const funcao4 = (objeto) => {
+    const string = []
+    string.push(objeto.Estilo)
+    return string
+  }
+  
+  console.log(funcao(serie4))
+
+
+
+
+
 
 //Quinta:
 
-const nomeSerie5 = "Friends"
-const genero5 = "Comédia"
-const duracaoTemp5 = Number(10)
-const anoLancamento5 = Number(1994)
-const remake5 = false
-const estiloSerie5 = ["Amigos", "festas", "paixões"]
-console.log(`Nome da Série: ${nomeSerie5.toUpperCase()}
-Gênero: ${genero5}
-Quantidade de Temporadas: ${duracaoTemp5}
-Ano de Lançamento: ${anoLancamento5}
-É remake de alguma série? ${remake5}
-Essa série gira em torno de: ${estiloSerie5}`)
+const serie5 = {
+Nome: "Friends",
+Gênero: "Comédia",
+Temporadas: Number(10),
+Lançamento: Number(1994),
+Remake: true,
+Estilo: "Amigos, festas, paixões"}
+
+// console.log("Nome da Série:", serie5.nomeSerie.toUpperCase(),
+// "Gênero:", serie5.genero,
+// "Quantidade de Temporadas:", serie5.duracaoTemp,
+// "Ano de Lançamento:", serie5.anoLancamento,
+// "É remake de alguma série?", serie5.remake,
+// "Essa série gira em torno de:", serie5.estiloSerie)
+
+for(let i in serie5){
+  console.log(i,serie5[i])}
+
+  const funcao5 = (objeto) => {
+    const string = []
+    string.push(objeto.Estilo)
+    return string
+  }
+  
+  console.log(funcao(serie5))
+
+
+
+
 
 //Cálculo da Média:
 
@@ -83,9 +166,74 @@ console.log("A média das temporadas de todas as séries em conjunto daria:", me
 
 //Verificação booleanas:
 
-const verificacao = remake1 && remake2 && remake3 && remake4 && remake5;
+const verificacao = serie1.Remake && serie2.Remake && serie3.Remake && serie4.Remake && serie5.Remake;
 console.log("Essas séries são remakes:", verificacao)
 
+let arraySeries = []
+
+if(serie1.Remake === true){
+  arraySeries.push(serie1.Nome)
+} else {
+  // alert("Item não será adicionado ao array")
+}
+
+if(serie2.Remake === true){
+  arraySeries.push(serie2.Nome)
+} else {
+  // alert("Item não será adicionado ao array")
+}
+
+if(serie3.Remake === true){
+  arraySeries.push(serie3.Nome)
+} else {
+  // alert("Item não será adicionado ao array")
+}
+
+if(serie4.Remake === true){
+  arraySeries.push(serie4.Nome)
+} else {
+  // alert("Item não será adicionado ao array")
+}
+
+if(serie5.Remake === true){
+  arraySeries.push(serie5.Nome)
+} else {
+  // alert("Item não será adicionado ao array")
+}
+console.log(arraySeries)
+
+
+const button = document.getElementById("Buscar")
+
+button.addEventListener("click", buscaPorNome)
+
+const buscaPorNome = (todasAsSeries, string) =>{
+  console.log(todasAsSeries)
+  console.log(string)
+  const resultado = todasAsSeries.filter((objeto) => {
+    return objeto.Nome.toLowerCase() === string
+  })
+  if(resultado.length > 0){
+    console.log(resultado)
+  } else {
+    alert("Nenhum item encontrado")
+  }
+}
+
+console.log(buscaPorNome(todasAsSeries, "Game of Thrones"))
+
+const busca = () => {
+  const input = document.querySelector(".input")
+  const valor = input.value.toLowerCase()
+  const listaSeries = document.querySelector(".arraySeries").querySelectorAll("section")
+  for (let i of listaSeries ) {
+    if(i.className === valor){
+      return i.innerHTML
+    }
+  }
+  alert("Não foi encontrada nenhuma série")
+  document.querySelector(".input").value=""
+}
 
 
 
@@ -93,10 +241,74 @@ console.log("Essas séries são remakes:", verificacao)
 
 
 
+// const botao = document.getElementById("Buscar")
+
+// const funcaoGeral = (array, string) => {
+//   console.log(array)
+//   console.log(string)
+  
+//       for(string of array){
+//    for(let i in array){
+//     console.log(i)
+//        if("i" === string){
+//          return i
+         
+//      } else {
+//        return("Não foi encontrado nenhum item")
+//      }
+     
+//   //  }
+//   }
+// }}
+// console.log(funcaoGeral(array, "Game of Thrones"))
+
+
+//  const selecionaFilmes = (filmes) => {
+//   const novaSelecao = arraySeries.filter((item) => {
+//     return (filmes === item)
+//   })
+//   console.log(novaSelecao)
+//  }
+
+
+ 
+
+
+
+// document.getElementById("Buscar").onclick = dofunction (funcaoGeral) 
+
+
+
+// {
+//   for(string of arraySeries){
+//     for(let i in arraySeries){
+//          if("i" === string){
+//            return i
+//        } else {
+//          return("Não foi encontrado nenhum item")
+//        }
+  
+//     }
+//   }
+
+//  };
 
 
 
 
 
 
-
+// const funcaoGeral = (arraySeries, string) => {
+//   for(nome of arraySeries){
+//   console.log("entrou no for nome")
+// //  for(let i in nome){
+// //   console.log("i", i)
+//    if(nome === string){
+//     console.log("nome", nome)
+//      return nome
+//    } else {
+//      return("Não foi encontrado nenhum item")
+//    }
+// //  }
+// }
+ 
